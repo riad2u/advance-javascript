f8764bc0790f19589802a28d1e878d397ae9f65f
Riazur Rahman <61244706+riad2u@users.noreply.github.com>
2020-08-07 02:27:47 +0600

    null vs undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,29 @@
+let pakhi;
+
+function add(num1, num2) {
+    console.log(num1 + num2);
+    return;
+}
+
+function add(num1, num2 = 10) {
+    total = (num1 + num2);
+    return total;
+}
+
+
+
+const result = add(5, 8);
+
+console.log(result);
+const premit = {
+    name: "smart pro",
+    phone: 157516
+}
+console.log(premit.gf);
+
+let fun = undefined;
+console.log(fun);
+
+
+let ages = [2, 3, 5];
+console.log(ages[11]);
