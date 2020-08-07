bffc2f71aa4ef724339b48e436ccfc4be333d969
Riazur Rahman <61244706+riad2u@users.noreply.github.com>
2020-08-07 19:36:12 +0600

    learn lots of new things

---
+++ map-filter.js
@@ -0,0 +1,36 @@
+const numbers = [3, 5, 6, 7, 8];
+
+// const output = [];
+// for (let i = 0; i < numbers.length; i++) {
+//     const element = numbers[i];
+//     const result = element * element;
+//     output.push(result);
+// }
+// console.log(output);
+
+
+
+
+// const result1 = numbers.map(function (element, index, array) {
+//     // return element * element;
+//     console.log(element, index, array)
+// })
+// console.log(result1);
+
+// const square1 = element => element * element;
+// console.log(square1);
+const square2 = numbers.map(x => x * x);
+console.log(square2);
+
+
+
+// const result = numbers.map(x => x * x);
+// console.log(result);
+// const isBiger = numbers.filter(x => x > 5);
+// console.log(isBiger);
+
+// const isBiger1 = numbers.filter(x => x < 3);
+// console.log(isBiger1);
+
+// const isThere = numbers.find(x => x > 5);
+// console.log(isThere);
---
+++ object.property.js
@@ -0,0 +1,21 @@
+const student = [{
+        id: 21,
+        name: "omar suny"
+    },
+    {
+        id: 32,
+        name: "Manna"
+    },
+    {
+        id: 31,
+        name: "depojol"
+    }, {
+        id: 53,
+        name: "Mahi"
+    }
+]
+const names = student.map(s => s.name);
+const ids = student.map(s => s.id);
+const bigger = student.filter(s => s.id > 40);
+const biggerOne = student.find(s => s.id > 30);
+console.log(biggerOne);
